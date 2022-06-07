ddb6d2b0cfa05cd1e4cc4f64574e31a2c4c6d886
Shriyansh Sharma <shris124@uw.edu>
2022-06-07 07:06:24 -0700

    Revert back to previous commit

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import Foundation
-import SystemConfiguration
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -19,65 +17,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let imageName: String
         let questions: [Question]
     }
-    var subjects: [Subject] = []
-//    var settings: UIAlertController!
-   /* var subjects: [Subject] = [
+
+    var settings: UIAlertController!
+    var subjects: [Subject] = [
         Subject(title: "Mathematics", description: "Test your mathematical skills", imageName: "math", questions: [Question(question: "What is 2 * 2", option1: "1", option2: "2", option3: "3", option4: "4", correct: 4), Question(question: "What is 32/16", option1: "4", option2: "16", option3: "2", option4: "8", correct: 3), Question(question: "What is the 42 + 6 * 3", option1: "84", option2: "50", option3: "60", option4: "70", correct: 3)]),
         Subject(title: "Marvel Super Heroes", description: "Test your marvel trivia skills", imageName: "marvel", questions: [Question(question: "How many infinity stones are there?", option1: "6", option2: "5", option3: "8", option4: "9", correct: 1), Question(question: "What is the name of the thor's hammer?", option1: "Mjolnir", option2: "Loki", option3: "Ragnarok", option4: "Time Stone", correct: 1), Question(question: "what is the first movie in time chronlogical order?", option1: "Avengers: Endgame", option2: "Ant-Man and the Wasp", option3: "Spiderman Homecoming", option4: "Captain America: The First Avenger", correct: 4)]),
         Subject(title: "Science", description: "Test your science skills", imageName: "science", questions: [Question(question: "What is the center of an atom", option1: "Nucleus", option2: "Hydrogen", option3: "Proton", option4: "Electron", correct: 1), Question(question: "How many hearts does an octupus have?", option1: "1", option2: "2", option3: "3", option4: "4", correct: 3), Question(question: "How many colors does a rainbow have?", option1: "4", option2: "5", option3: "6", option4: "7", correct: 4)]),
-        ]*/
+        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        settings = UIAlertController(title: "Settings", message: "Settings go here/OK", preferredStyle: .alert)
         table.delegate = self
         table.dataSource = self
         // Do any additional setup after loading the view.
     }
     
     @IBAction func clickSettings(_ sender: Any) {
-        let settings = UIAlertController(title:"Settings", message: "Retrieve data by inputting url", preferredStyle: .alert)
-        settings.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-        settings.addTextField(configurationHandler: { textField in
-        textField.placeholder = "Input your url to retrieve data"
-        })
-
-        settings.addAction(UIAlertAction(title: "Check now", style: .default, handler: { action in
-        if let url = settings.textFields?.first?.text {
-            self.loadData(urlInput: url)
-        }
-        }))
-        
         self.present(settings, animated: true)
-    }
-//Refereced from StackOverFlow Article https://stackoverflow.com/questions/25398664/check-for-internet-connection-availability-in-swift
-    class func isConnectedToNetwork() -> Bool {
-
-        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
-        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-
-        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
-        }
-
-        var flags: SCNetworkReachabilityFlags = 0
-        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
-            return false
-        }
-
-        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-
-        return isReachable && !needsConnection
-    }
-    
-    func loadData(urlInput: String) {
-        let request = URLRequest(url: URL(string: urlInput)!)
-        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-        let fileName = "data.json"
-        
-        
     }
     
     
